2040345c0088caa27afbc1a692db4f7b625837c0
vamik <vamiksiddiqui666@gmail.com>
2024-08-31 19:18:19 +0530

    changes in frontend

---
+++ frontend/src/components/components.js
@@ -0,0 +1,24 @@
+import LoadingSpinner from "./common/LoadingSpinner";
+import Post from "./common/Post";
+import Posts from "./common/Posts";
+import ProfileHeaderSkeleton from "./common/ProfileHeaderSkeleton";
+import RightPanel from "./common/RightPanel";
+import Sidebar from "./common/Sidebar";
+
+import PostSkeleton from "./skeletons/PostSkeleton";
+import RightPanelSkeleton from "./skeletons/RightPanelSkeleton";
+
+import XSvg from "./svgs/X";
+
+
+export {
+    LoadingSpinner,
+    Post,
+    Posts,
+    ProfileHeaderSkeleton,
+    RightPanel,
+    Sidebar,
+    PostSkeleton,
+    RightPanelSkeleton,
+    XSvg
+}
---
+++ frontend/src/pages/pages.js
@@ -0,0 +1,16 @@
+import HomePage from "./home/HomePage";
+import LoginPage from "./auth/login/LoginPage";
+import SignUpPage from "./auth/signup/SignUpPage";
+import NotificationPage from "./notification/NotificationPage";
+import EditProfileModal from "./profile/EditProfileModal";
+import ProfilePage from "./profile/ProfilePage";
+
+
+export {
+    HomePage,
+    LoginPage,
+    SignUpPage,
+    NotificationPage,
+    EditProfileModal,
+    ProfilePage
+}
